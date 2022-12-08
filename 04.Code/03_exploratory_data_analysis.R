c84814c7cc0a328069b37ef967754a2129718bbd
hje-mgomd <67647498+alphabetac@users.noreply.github.com>
2022-12-08 10:09:18 +0000

    Update 03_exploratory_data_analysis.R

--- 04.Code/03_exploratory_data_analysis.R
+++ 04.Code/03_exploratory_data_analysis.R
@@ -44,7 +44,7 @@ campaign_end_date <- as.Date("2022-11-13")
 ## PLOT DATA --------------------------
 
 ## Distribution of conversions
-business_conversions_long %>%
+business_demo_requests_long %>%
   ggplot(aes(x = goal10Completions, fill = countryIsoCode, colour = countryIsoCode)) +
   geom_histogram(aes(y = ..density..),
                  binwidth = 1) +
@@ -52,13 +52,32 @@ business_conversions_long %>%
   facet_wrap(~countryIsoCode)
 
 ## Time series
-business_conversions_long %>%
+demo_requests_time_series <- business_demo_requests_long %>%
+  filter(date <= as.Date("2022-10-01")) %>%
   ggplot(aes(x = date, y = goal10Completions)) +
   geom_smooth(aes(linetype = countryIsoCode,
                   colour = countryIsoCode),
               method = 'gam',
               se = F) +
   geom_vline(aes(xintercept = as.Date("2022-10-02"))) +
+  labs(x = "\nDate\n",
+       y = "Demo Requests\n",
+       linetype = "Country (Iso Code)",
+       colour = "Country (Iso Code)") +
+  hj_theme()
+
+free_signups_time_series <- business_free_signups_long %>%
+  filter(date <= as.Date("2022-10-01")) %>%
+  ggplot(aes(x = date, y = goal6Completions)) +
+  geom_smooth(aes(linetype = countryIsoCode,
+                  colour = countryIsoCode),
+              method = 'gam',
+              se = F) +
+  geom_vline(aes(xintercept = as.Date("2022-10-02"))) +
+  labs(x = "\nDate\n",
+       y = "Free Sign-ups\n",
+       linetype = "Country (Iso Code)",
+       colour = "Country (Iso Code)") +
   hj_theme()
 
 ## Time series
@@ -82,3 +101,16 @@ business_free_signups_long %>%
   hj_theme()
 
 ## CORRELATIONS -----------------------
+
+
+## SAVE PLOTS -------------------------
+
+png(file = here("05.Output/figures/demo_requests_time_series.png"),
+    width = 1200)
+demo_requests_time_series
+dev.off()
+
+png(file = here("05.Output/figures/free_signups_time_series.png"),
+    width = 1200)
+free_signups_time_series
+dev.off()
